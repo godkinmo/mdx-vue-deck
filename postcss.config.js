8d746c7219d666bd4b6968e66d93499a50fcabd0
Jack Fong <godkin.mo@gmail.com>
2019-09-03 20:31:23 +0800

    Add img tag in whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -11,6 +11,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+
+  whitelist: ['img'],
 })
 
 module.exports = {
