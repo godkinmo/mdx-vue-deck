efd88d1cfe22af6c0a6bc3e039928a215acf3738
Jack Fong <godkin.mo@gmail.com>
2019-09-15 02:41:12 +0800

    Fix code without classname of the language

--- markdown-loader.js
+++ markdown-loader.js
@@ -4,16 +4,28 @@ module.exports = function (src) {
   src = src.replace('wrapper', 'div')
   src = src.replace(/inlineCode/g, 'code')
 
-  // Transform markdown img tag
-  const re = new RegExp(`(<img parentName="p".*\n.*\n.*\n.*</img>)`, 'g');
-  src = _.replace(src, re, replacement => {
+  /* eslint-disable-next-line */
+  const imgRe = new RegExp(`(<img parentName="p"(.|\r|\n)*?<\/img>)`, 'g');
+  src = _.replace(src, imgRe, replacement => {
     const imgRe = /"src": "(.*)"/g
     const altRe = /"alt": "(.*)"/g
 
     const imgUrl = (imgRe.exec(replacement))[1]
     const imgAlt = altRe.exec(replacement)
 
-    return  `<img src="${imgUrl}" alt="${imgAlt ? imgAlt[1] : null}" />`
+    return `<img src="${imgUrl}" ${imgAlt ? `alt="${imgAlt[1]}"` : ''} />`
+  })
+
+  /* eslint-disable-next-line */
+  const codeRe = new RegExp(`(<code parentName="pre"(.|\r|\n)*?<\/code>)`, 'g');
+  src = _.replace(src, codeRe, replacement => {
+    const classNameRe = /"className": "(.*)"/g
+    const className = classNameRe.exec(replacement)
+
+    const codeRe = /<code parentName="pre"(.|\r|\n)*?>({`(.|\r|\n)*?`})<\/code>/g
+    const code = (codeRe.exec(replacement))[2]
+
+    return `<code ${className ? `class='${className[1]}'` : ''}>${code}</code>`
   })
 
   return src
