8c72ee37e4e6919daa67891ff0b13f248a6b4a51
Jack Fong <godkin.mo@gmail.com>
2020-01-26 18:11:45 +0800

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-unused-components': 1
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
